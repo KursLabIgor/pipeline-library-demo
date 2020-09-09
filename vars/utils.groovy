bd9f0755ac9ec43384be328d8f725c90500169a7
Ihor <lil2006@inbox.ru>
2020-09-09 12:55:11 +0300

    replace function

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -1,6 +1,10 @@
 #!/usr/bin/env groovy
 
-def name (String name = 'human') {
-  echo "Hello, ${name}."
+def info(message) {
+    echo "INFO: ${message}"
+}
+
+def warning(message) {
+    echo "WARNING: ${message}"
 }
 
